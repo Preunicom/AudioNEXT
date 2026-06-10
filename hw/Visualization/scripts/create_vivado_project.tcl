68b44bf4e24fed19165800ee46ac6195ab7aa780
Preunicom <44133097+Preunicom@users.noreply.github.com>
2026-06-10 08:21:51 +0200

    Reworked create_vivado_project tcl script

--- hw/Visualization/scripts/create_vivado_project.tcl
+++ hw/Visualization/scripts/create_vivado_project.tcl
@@ -58,7 +58,7 @@ set sim_files [concat \
     [find_files $general_files_dir/sim "*.sv"] \
 ]
 set sim_bd_files [concat \
-    [find_files $general_files_dir/sim "*.bd"] \
+    [find_files $general_files_dir/sim/bd "*.bd"] \
 ]
 set constr_file [lindex [find_files $project_data_dir/constraints "*.xdc"] 0]
 
@@ -135,3 +135,20 @@ set_property -name "xsim.simulate.runtime" -value "0ns" -objects $obj
 # ================ IP REPO ================
 set_property ip_repo_paths [list $ip_repo_path] [current_project]
 update_ip_catalog
+
+# ================ BD WRAPPER ================
+# Do this as last step because if IPs in the BD are locked the script fails at this point.
+
+set obj [get_filesets sources_1]
+foreach file $bd_files {
+    set file_obj [get_files $file]
+    set wrapper_file [make_wrapper -files $file_obj -top]
+    add_files -norecurse -fileset $obj $wrapper_file
+}
+
+set obj [get_filesets sim_1]
+foreach file $sim_bd_files {
+    set file_obj [get_files $file]
+    set wrapper_file [make_wrapper -files $file_obj -top]
+    add_files -norecurse -fileset $obj $wrapper_file
+}
